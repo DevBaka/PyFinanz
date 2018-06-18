08d94e6c291f5759e68b35dc18f8b9039067b265
DevBaka <devbaka@gmx.net>
2018-06-18 14:09:40 +0200

    first button works

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
 
 
@@ -9,7 +10,20 @@ class mainWindow(object):
         #mW.setObjectName("Moomas")
         mW.setGeometry(50,50,500,500)
         mW.setWindowTitle("Moomas Finanzen")
-        mW.setWindowIcon(QIcon("static/RDS.png"))        
+        mW.setWindowIcon(QIcon("static/RDS.png"))
+        self.test(mW)
+        
+    def test(self, mW):
+        print("hello")
+        cmdTest = QPushButton("Quit", mW)
+        cmdTest.clicked.connect(self.close_application)
+        cmdTest.resize(100,100)
+        cmdTest.move(100,100)
+        #self.show()
+        
+    def close_application(self):
+        print("whoaa shutdown app!!!")
+        sys.exit()
 
 
 
